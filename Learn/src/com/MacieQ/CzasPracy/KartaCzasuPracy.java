6fc8feea2f00a9300ab2a9406fcf3b78c9318350
MacieQ <macieq84@gmail.com>
2018-08-22 07:25:16 +0200

    add karta czasy pracy - empty

---
+++ Learn/src/com/MacieQ/CzasPracy/KartaCzasuPracy.java
@@ -0,0 +1,19 @@
+package com.MacieQ.CzasPracy;
+
+/**
+ * Created by MacieQ
+ * E-mail: macieq84 on Google
+ * Date: 2018-08-22
+ */
+public class KartaCzasuPracy {
+/*
+    31 dni z zaznaczeniem weekendów
+    między nr dnia, a ilością godz komórka z symbolem do wyboru
+    komórki dni godzinowe
+    na końcu
+    komórka z normą czasu pracy
+    suma godzin przepracowanych
+            różnica
+*/
+
+}
